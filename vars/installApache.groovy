65d52d92ca513b89a9f651c4553920c626da4058
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-03-27 12:18:08 +0530

    Update installApache.groovy

--- vars/installApache.groovy
+++ vars/installApache.groovy
@@ -1,6 +1,6 @@
-def installApache() {
+// vars/installApache.groovy
+def call() {
     echo "Executing Ansible playbook to install Apache..."
     sh 'ansible-playbook -i /etc/ansible/hosts vars/apache_setup.yaml'
     echo "Apache installation completed!"
 }
-
