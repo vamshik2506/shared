dfe5c56b23b82ef2396f5e1cccebe3eaaa53651e
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-03-27 12:23:48 +0530

    Update installApache.groovy

--- vars/installApache.groovy
+++ vars/installApache.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "Executing Ansible playbook to install Apache..."
     
-    // Update the path to ansible/apache_setup.yaml
-    sh 'ansible-playbook -i /etc/ansible/hosts ansible/apache_setup.yaml'
+    // Run Ansible with sudo and specify the user
+    sh 'ansible-playbook -i /etc/ansible/hosts ansible/apache_setup.yaml --become --become-user=root'
 }
